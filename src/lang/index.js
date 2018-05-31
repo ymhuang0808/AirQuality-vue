53dbc0a03898e4be851da272485e4b634cc306ba
John Huang <ymhuang@fmbase.tw>
2018-05-31 09:51:44 +0800

    Bug: fix lazy loading for translations

---
+++ src/lang/index.js
@@ -0,0 +1,9 @@
+import enUS from './en-US'
+import zhTW from './zh-TW'
+
+const messages = {
+  'en-US': enUS,
+  'zh-TW': zhTW
+}
+
+export default messages
--- src/setup/i18n-setup.js
+++ src/setup/i18n-setup.js
@@ -1,21 +1,19 @@
 import Vue from 'vue'
 import store from '../store/index'
 import VueI18n from 'vue-i18n'
+import messages from '@/lang'
 
 Vue.use(VueI18n)
 
 export const i18n = new VueI18n({
   locale: store.getters.lang == null ? navigator.language : store.getters.lang,
   fallbackLocale: 'en-US',
-  messages: {
-    'en-US': require('../lang/en-US.json'),
-    'zh-TW': require('../lang/zh-TW.json')
-  }
+  messages
 })
 
 export const availableLanguages = ['en-US', 'zh-TW']
 
-const loadedLanguages = ['en-US']
+const loadedLanguages = ['zh-TW']
 
 function getLanguageCode (lang) {
   if (lang.indexOf('-')) {
@@ -34,8 +32,13 @@ function setI18nLanguage (lang) {
 
 export function loadLanguageAsync (lang) {
   if (i18n.locale !== lang) {
-    if (!loadedLanguages.indexOf(lang)) {
+    console.log('loadedLanguages...')
+    console.log(loadedLanguages)
+    console.log('lang = ' + lang)
+    if (!loadedLanguages.includes(lang)) {
+      console.log('load ' + lang)
       return import(/* webpackChunkName: "lang-[request]" */ `@/lang/${lang}`).then(msgs => {
+        console.log('loaded ' + lang + ' successfully')
         i18n.setLocaleMessage(lang, msgs.default)
         loadedLanguages.push(lang)
         return setI18nLanguage(lang)
